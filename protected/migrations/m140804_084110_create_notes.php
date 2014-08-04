7d2f3ccc44ac8c1357b268d5f12b86136da2f317
Andrew <komand9@gmail.com>
2014-08-04 17:47:06 +0300

    update migrations
    fixed types and existing tables

--- protected/migrations/m140804_084110_create_notes.php
+++ protected/migrations/m140804_084110_create_notes.php
@@ -4,22 +4,31 @@ class m140804_084110_create_notes extends CDbMigration
 {
 	public function up()
 	{
-		$this->createTable('notes', array(
-			'id_Note' => 'pk',
-			'Author' => 'CHAR(255) NOT NULL',
-			'Title' => 'CHAR(255) NOT NULL',
-			'Content' => 'TEXT NOT NULL',
-			'Date' => 'DATETIME',
-		),'ENGINE InnoDB  CHARACTER SET=UTF8');
-		$this->createTable('comments', array(
-			'id' => 'pk',
-			'Author' => 'CHAR(255) NOT NULL',
-			'Content' => 'TEXT NOT NULL',
-			'Date' => 'DATETIME',
-			'id_Note' => '',
-		),'ENGINE InnoDB  CHARACTER SET=UTF8');
-		$this->addForeignKey('FK_comment_note', 'comments', 'id_Note', 
-			'notes', 'id_Note', 'CASCADE', 'CASCADE');
+		if (Yii::app()->db->schema->getTable('notes',true)===null) {
+			$this->createTable('notes', array(
+				'id_Note' => 'pk',
+				'Author' => 'CHAR(255) NOT NULL',
+				'Title' => 'CHAR(255) NOT NULL',
+				'Content' => 'TEXT NOT NULL',
+				'Date' => 'DATETIME',
+			),'ENGINE InnoDB  CHARACTER SET=UTF8');
+		} else {
+			echo "table notes already exist\n";
+		}
+
+		if (Yii::app()->db->schema->getTable('comments',true)===null) {
+			$this->createTable('comments', array(
+				'id' => 'pk',
+				'Author' => 'CHAR(255) NOT NULL',
+				'Content' => 'TEXT NOT NULL',
+				'Date' => 'DATETIME',
+				'id_Note' => 'INT',
+			),'ENGINE InnoDB  CHARACTER SET=UTF8');
+			$this->addForeignKey('FK_comment_note', 'comments', 'id_Note', 
+				'notes', 'id_Note', 'CASCADE', 'CASCADE');
+		} else {
+			echo "table comments already exist\n";
+		}		
 	}
 
 	public function down()
